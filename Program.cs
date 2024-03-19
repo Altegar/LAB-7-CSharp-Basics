f6c3774d3c45db1cc352ac60663b2fd8bcef4aa6
Altegar <91095463+Altegar@users.noreply.github.com>
2024-03-19 23:10:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Сушинський Ігор
+// Лабораторна робота № 7
+// Літерні рядки.
+// Варіант 3
+
+namespace LAB_7_CSharp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("s = ");
+            string s = Console.ReadLine();
+            s = s.Replace('-', '+');
+            Console.WriteLine(s);
+        }
+    }
+}
